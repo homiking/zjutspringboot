c0deefb3f6b0069798228eb5b7b9b419b0dd773b
kknever <942804699@qq.com>
2022-06-17 21:25:17 +0800

    增加了test2

--- src/main/java/com/zhm/drug/Test.java
+++ src/main/java/com/zhm/drug/Test.java
@@ -17,6 +17,6 @@ import java.util.function.Function;
  **/
 public class Test {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(2);
     }
 }
---
+++ src/main/java/com/zhm/drug/Test2.java
@@ -0,0 +1,12 @@
+package com.zhm.drug;
+
+/**
+ * @Description
+ * @Author kknever
+ * @Date 2022/6/13
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println(2);
+    }
+}
